dc145cfde8a75abc4108d855d2b44a3615953438
HILLELOH <hilleloh@gmail.com>
2022-12-18 06:35:46 +0200

    add toolbar and new sign_up activity

--- app/src/main/java/AnimEngine/mobile/LoginActivity.java
+++ app/src/main/java/AnimEngine/mobile/LoginActivity.java
@@ -3,12 +3,76 @@ package AnimEngine.mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editTextEmail, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.text_link_forgot_password).setOnClickListener(this);
+        findViewById(R.id.text_link_forgot_password).setOnClickListener(this);
+        findViewById(R.id.button_login).setOnClickListener(this);
+
+
+        TextInputLayout mi = findViewById(R.id.text_input_email_login);
+        editTextEmail = mi.getEditText();
+
+        TextInputLayout pi = findViewById(R.id.text_input_password_login);
+        editTextPassword = pi.getEditText();
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == findViewById(R.id.text_link_forgot_password)){
+            BottomSheetDialog bsd = new BottomSheetDialog(this);
+            bsd.setContentView(R.layout.bottom_sheet_forgot_password);
+            bsd.show();
+            Button submit = bsd.findViewById(R.id.button_submit);
+            submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        if(view == findViewById(R.id.button_login)){
+            String mailInput = editTextEmail.getText().toString();
+            Pattern pattern = Pattern.compile("([a-zA-Z0-9]+)([_.-]{1}[a-zA-Z0-9]+)*@([a-zA-Z0-9])+(-{1}[a-zA-Z0-9]+)*(\\.([a-zA-Z]{2,}))+", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(mailInput);
+            if(matcher.matches()){
+                Toast.makeText(getApplicationContext(),"you ok",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"Invalid Email",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        else{
+            System.out.println("hi");
+        }
     }
 }
---
+++ app/src/main/java/AnimEngine/mobile/RegisterActivity.java
@@ -0,0 +1,38 @@
+package AnimEngine.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        findViewById(R.id.text_link_sign_in).setOnClickListener(this);
+
+    }
+
+
+
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == findViewById(R.id.text_link_sign_in)) {
+            Intent ActivitysignIn = new Intent(this, LoginActivity.class);
+            startActivity(ActivitysignIn);
+        }
+    }
+}
